1a054f0c43115f2607f5907995719c4d3deac86c
evanlissoos <evanlissoos@gmail.com>
2016-07-23 11:05:12 -0700

    Added new keys to server code

--- server/metric.py
+++ server/metric.py
@@ -21,7 +21,7 @@ inputs_scrypt = ['DGB', 'GLD', 'CNC', 'NVC', 'GAME']
 inputs_sha = ['PPC', 'BTC', 'ZET', 'MZC', 'TEK']
 expcoin_historical = []
 
-hashrates = {'DGB' : 100, 'GLD' : 100, 'CNC' : 100, 'NVC' : 100, 'GAME' : 100, 'PPC' : 100, 'BTC' : 100, 'ZET' : 100, 'MZC' : 100, 'TEK' : 100}
+hashrates = {'DGB' : 0.002, 'GLD' : 0.002, 'CNC' : 0.002, 'NVC' : 0.002, 'GAME' : 0.002, 'PPC' : 1.4, 'BTC' : 1.4, 'ZET' : 1.4, 'MZC' : 1.4, 'TEK' : 1.4}
 exchange_rates_q = multiprocessing.Queue()
 block_rewards_q = multiprocessing.Queue()
 difficulties_q = multiprocessing.Queue()
@@ -30,58 +30,56 @@ metrics = dict.fromkeys(inputs, 0)
 volatilities = dict.fromkeys(inputs_scrypt, 0)
 
 apiCounters = {'DGB' : 0, 'GLD' : 5, 'CNC' : 10, 'NVC' : 15, 'GAME' : 20, 'PPC' : 25, 'BTC' : 30, 'ZET' : 35, 'MZC' : 40, 'TEK' : 45}
-apikeys = ['520c4970e88949ba81e9db583711d439',
-	'8d98a4fe80854fdebef12b7b758e9aec',
-	'850e29b35e0042ee9c2f230e795db278',
-	'1df54139e3924f5e8ebd9b3a96110462',
-	'3859079deb804e5995f0f5e61215d4b9',
-	'037a2bef22e549ef80c69d6a70a90ea6',
-	'c0ff9ad2c6fc48cf8743bff2335e17d4',
-	'5f03d75ee98f43adbad412ccdbdf9503',
-	'bc6dd350dd7a4ebab43582f56f89882f',
-	'cbdedc14aa904c30a7a8be7faf248d74',
-	'e21225fc059b47d19c841b0d52b646e7',
-	'f6343455cdfd427189a20dd9d32b3120',
-	'0c1f733077ec4de39510d9e41174c780',
-	'a7841786bc4c4da5afd75db4b2574c74',
-	'5e68df33bdba4cbdaeabac20bfec2bce',
-	'ebe42595f7174212b204f0cdb92b21da',
-	'acc7fa8e448a4d1db6c058bd6bd67243',
-	'd999af13466d4553b306396fff12bdf7',
-	'b0fc0ea5a5e84aab9a50b978fa256faa',
-	'e82a4ea0dc384f1b8a8a5c3414d932cf',
-	'f108dd6f22ba447baa2fe10f2fb58791',
-	'e92663f4ab7640ae83e53dc15d9556dc',
-	'9a2042c9e37546d9a4aecade9edede29',
-	'3629dc9f6aac499ea5fa5b50b599072d',
-	'7165cfb39e4c42aea0268a6daabf2c41',
-	'745b26f5e94841819474d0db9e013dff',
-	'478d14b68d3f46bf8ab5e0b87a253a24',
-	'b4d38b0776ed4a3bb6d8168ec79c7761',
-	'1cdf321522524154978fbf6e4781cadf',
-	'7fafe7ff418a41ec8f98861291c773bc',
-	'04b3859baa424d38881e4e0b08eea123',
-	'b5e248c9640843ce9e4a82f677c81ee8',
-	'8e0383fc5d25440496d7bd35c97ec8d0',
-	'ed1dcfee670b4c0f96d45ec6a166f0d1',
-	'9a1805fa5f5640df8239f6e3fc58a721',
-	'3f165243e5954c04920f41f8ed71e2eb',
-	'7d1000b6ca304353bcadc3d3396b2be4',
-	'1eb61ba3976847faaeba6de118524000',
-	'f3d6df1e3ee14ca1994c4fc5b73d87c2',
-	'1eb8aace7c99478dadf8ec5582dcdc77',
-	'ac3c3abd06fb465f9cade5e58172bed1',
-	'18b90175f430451f850e838d5a1dee01',
-	'7b95a0e6661b43acb4180d0631e9f424',
-	'2dafc495462d4f6ba25748a96d3e7829',
-	'2526820e7f974890a4dad5b1b3fbbe5b',
-	'4a75e695fcd34046bb919e8f84aebf6e',
-	'3b32884368514179accaec6c54637a5b',
-	'9d53cf609d244c3099ffee52bf0d508b',
-	'c4d2517baec34c5da2b1c489c0ce7835',
-	'6481f6e98263406091fa58864e9872cb',
-	'03f8bb8be9a94a3888482077f2fb8b62',
-	'cba7de5a43cf43aea5e69e531dc9c5c4',]
+apikeys = ['46fc41e61fed403aa3e88b2a5e71f48f'
+	'c32104fcbf4042298fcaccfc73469bc2'
+	'3a89841177af4a369cab3f2182e7715b'
+	'05e1e4da564b4f12b82360d438eb1c6b'
+	'd959ed79ef54467293952d882d4e37a4'
+	'9b6f4fdc67444fce8144dee922046259'
+	'b2f42e6ad3504b7cbaf27caa6863fe02'
+	'de3a314e61be468f8e877d57b4f189d4'
+	'b45a5549a28d4cc983f272e09775bdba'
+	'e8cdaadba7204900bcb263dfb758243b'
+	'21e4ad8d24c24528b805715bfe3650c4'
+	'3cac67f5035f46d5acd81213f09f6f57'
+	'6d7121712f0c47ec8d6816c4691e0d5d'
+	'6983b8c7d888438bb6a7fd0864267734'
+	'a18e92fa6990459f9cb46921d559d2e2'
+	'460b21f06f224e60af8ccadc093329cd'
+	'90b6f192659646b3939476b1c9dd7a4b'
+	'f5817103d6a6495a9049654b5a986d23'
+	'573d632f2c954f1bb06728c1013222dd'
+	'4bbc9a2bc912493bb269a381cb2af2d2'
+	'0a49f8e2d774455ead345f1a62fcc3fb'
+	'5c8228f9786d43d195a927a4ff26b403'
+	'62c2f90cd7bb4d7c8f6e668d4ee9582b'
+	'f9e5ac48975b423c881158fa0cb143cf'
+	'd8af95ac41f6492f8b8a4bdb540708d0'
+	'd92aeb4cc52442ee8b6ae73663efe9b1'
+	'071d1142349347a8bbb73b0b6ee5ff53'
+	'e2ae7800f92446caa26375791c94f1ca'
+	'a06e3c143a854f0184f1874bed9ce974'
+	'eaacc9bbcafd4bda884c0da3fa9d2d00'
+	'e93e12d057284ff39dfb84291fc4e104'
+	'8778dd8759694873966a2034e73b0a04'
+	'e9f7bce88e7e4e20ac90cdfa70ca45b9'
+	'c4e5f827c23247e895543b5a475421ed'
+	'33d6e8d4de0d4df5bb8c237b1564981e'
+	'ae3704905bd840e288c248ae9a063713'
+	'69063c7e2d2c4cbc9711e8ce2196c338'
+	'e9ed6ea8e212438f9677e39b70b495cb'
+	'343e39304569423b910e8d258901b5e2'
+	'b9e140e52a974c429e2d4c35bf2e7a87'
+	'a86f65496eb049b4a0d2299ab45c59e3'
+	'ccf52127d4894fa4a7de4b6bac21d205'
+	'418da6f76f7d4926bc859234bf7bded2'
+	'279bbf8fb99f4b62ba16a26c9de1166d'
+	'981670b01ccc4630a63631f35acdf6ff'
+	'b635922825d445cd9ab391693ce2dfaa'
+	'82809b7bf41f440da55e1274446b066c'
+	'4bdf3c604ccf4e77a919491d58faaecb'
+	'15372b9141f54643b0740ec57f24f141'
+	'ee55a6712bb04996af0935d2b84eb7f0']
 
 ###########################FUNCTIONS###########################
 def estimate(i):
